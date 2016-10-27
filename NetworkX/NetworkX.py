5ffaa768aa00f6ad3b3085a97ec2e9f1e0c80ec4
sven <sven.fritz@hotmail.de>
2016-10-27 20:46:03 +0200

    final

--- NetworkX/NetworkX.py
+++ NetworkX/NetworkX.py
@@ -1,49 +1,109 @@
 #!/usr/bin/env python
 
 import networkx as nx
-import igraph as ig
 import matplotlib.pyplot as plt
-import random 
+import random
+import pandas as pd
 
-"create Graph using NetworkX:"
+# Undirected Graph:
 G = nx.Graph()
-i = 0
-for i in range(0, 10):
-    G.add_edge(random.randrange(0,10,1), random.randrange(0,10,1))
-"is G connected?"
-print(nx.is_connected(G))
-"plot"
+# create nodes:
+List_of_nodes = [1, 2, 3, 5, "Jonas", "Sven"]
+G.add_nodes_from(List_of_nodes)
 nx.draw_networkx(G)
-plt.title("Randomly created graph (10 edges)")
 plt.show()
 	
-
-"visualize example data graph:"
-H = nx.read_gml("/home/sven/Downloads/dolphins.gml", relabel = True)
-print(nx.is_connected(H))
-nx.draw_networkx(H)
-plt.title("Graph representation of interaction between dolphins")
+# create edges:
+List_of_edges = [(1,2), (2,"Jonas"), (1,3), (3,5), (2,3), ("Sven",1), ("Sven",2), ("Sven",3), ("Sven",5)]
+G.add_edges_from(List_of_edges)
+nx.draw_networkx(G)
 plt.show()
 
-"""try igraph for same visualization:"
-G = ig.load("/home/sven/Downloads/dolphins.gml")
-ig.plot(G)
-"""
+# remove nodes:
+G.remove_node(2)
+nx.draw_networkx(G)
+plt.show()
 
-"""
-# Graph example
-# create empty Graph
-G = nx.Graph()
-# add node(s)
-G.add_nodes_from([3,4,5,6])
-# add edge(s)
-G.add_edges_from([(4,5), (3,6), (4,6)])
+# create random graph:
+H = nx.Graph()
+# Graph can be created by only adding edges:
+for i in range(0, 10):
+    H.add_edge(random.randrange(0,10,1), random.randrange(0,10,1))
+nx.draw_networkx(H)
+plt.show()
+print("number of edges in H: %d" % nx.number_of_edges(H))
+print(H.nodes())
+print(H.edges())
 
-# inspect Graph
-#print(G.number_of_nodes())
-#print(G.number_of_edges())
-print("neighbors of 6:")
-print(G.neighbors(6))
-print("degree of G:")
-print(nx.degree(G))
-"""
+# Directed Graph:
+I = nx.DiGraph()
+List_of_edges = [("Max","Peter"), ("Peter","Jonas"), ("Max","Till"), ("Till","Luisa"), ("Jonas","Luisa"), 
+                 ("Peter","Till"), ("Sara","Max"), ("Sara","Peter"), ("Sara","Till"), ("Sara","Luisa")]
+I.add_edges_from(List_of_edges)
+nx.draw_networkx(I)
+plt.title("distribution of exercise solutions through students")
+plt.show()
+
+# MultiDiGraph:
+J = nx.MultiDiGraph()
+J.add_edges_from(I.edges())
+J.add_edge("Sara","Peter")
+nx.draw_networkx(J)
+plt.title("distribution of exercise solutions through students")
+plt.show()
+print(J.edges())
+print(I.edges())
+
+### ANALYZING NETWORKS USING NETWORKX ###
+
+# create undirected network from I:
+I = I.to_undirected()
+nx.draw_networkx(I)
+plt.title("example social network")
+plt.show()
+# analyze graph I
+print("number of nodes in I: %d" % I.number_of_nodes())
+print("number of edges in I: %d" % I.number_of_edges())
+print("Sara is friends with:") 
+print(I.neighbors("Sara"))
+print("degree of nodes in I:")
+I.degree()
+print("Is I connected? %s" % nx.is_connected(I))
+print("Is I directed? %s" % nx.is_directed(I))
+
+# adjacency list of I:
+print(I.nodes())
+I.adjacency_list()
+
+### NETWORK ALGORITHMS ON EXAMPLE DATA ###
+
+# metrics on example social network:
+b = nx.betweenness_centrality(I, k = None, normalized = True, endpoints = False, seed = None)
+c = nx.closeness_centrality(I)
+d = nx.degree_centrality(I)
+d = {"Betweenness": b, "Closeness": c, "Degree": d}
+nx.draw_networkx(I)
+plt.title("example social network")
+plt.show()
+pd.DataFrame(d)
+
+###
+# example data:
+# D. Lusseau, K. Schneider, O. J. Boisseau, P. Haase, E. Slooten, and S. M. Dawson, 
+# Behavioral Ecology and Sociobiology 54, 396-405 (2003)
+###
+K = nx.read_gml("dolphins.gml", relabel = True)
+b = nx.betweenness_centrality(K)
+c = nx.closeness_centrality(K)
+d = nx.degree_centrality(K)
+dic = {"Betweenness": b, "Closeness": c, "Degree": d}
+data = pd.DataFrame(dic)
+betweenness_top = data.sort_index(by = "Betweenness", ascending = False)[0:10].index
+closeness_top = data.sort_index(by = "Closeness", ascending = False)[0:10].index
+degree_top = data.sort_index(by = "Degree", ascending = False)[0:10].index
+dic_top = {"Betweenness top": betweenness_top, "Closeness top": closeness_top, "Degree top": degree_top}
+
+nx.draw_networkx(K)
+plt.title("Social network of dolphins")
+plt.show()
+print(pd.DataFrame(dic_top, index = range(1,11)))
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # IgraphNetworkXIntroduction
+
+A basic introduction to the python packages available for network creation, manipulation and analyzation: NetworkX and Igraph. Both packages are used in Python 2.7.
+
+# data 
+
+The folder [Igraph](https://github.com/AuelJonas/IgraphNetworkXIntroduction/tree/master/Igraph "Igraph") contains a jupyter notebook with basic network operations using igraph. If you don't have jupyter notebook installed, you can also use the [igraph_python.py](https://github.com/AuelJonas/IgraphNetworkXIntroduction/blob/master/Igraph/igraph_python.py "igraph_python.py") file. Additionally there is a real world data network polblogs.gml which is used in the python scripts.
+
+The [NetworkX](https://github.com/AuelJonas/IgraphNetworkXIntroduction/tree/master/NetworkX "NetworkX") folder similary contains a NetworkX jupyter notebook and the corresponding [NetworkX.py](https://github.com/AuelJonas/IgraphNetworkXIntroduction/blob/master/NetworkX/NetworkX.py "NetworkX.py"). As a real world data network dolphins.gml is used in the python scripts. 
+
+The real world datasets are taken from:
+http://www-personal.umich.edu/~mejn/netdata/
+
+The folder [Praesentation](https://github.com/AuelJonas/IgraphNetworkXIntroduction/tree/master/Praesentation "Praesentation") contains a beamer presentation about these two packages with some useful references.
+
+# usage
+
+To run the .py files in your linux terminal use
+<pre><code> python igraph_python.py </code></pre>
+or
+<pre><code> python NetworkX.py </code></pre>
+in the corresponding folder.
+
+If you'd rather use the jupyter notebooks, you can easily install it here:
+https://www.continuum.io/downloads .
+
+After installation just run 
+<pre><code> jupyter notebook </code></pre>
+in your terminal to open a local notebook server in your browser. You can then select the notebooks provided and run them.
